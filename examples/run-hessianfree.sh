cc808803e69ffcdcdcf4d34e79151463d6be198c
Simon Fetzel <simonf93@univie.ac.at>
2024-07-30 17:05:46 +0000

    corrected hessianfree run script

--- examples/run-hessianfree.sh
+++ examples/run-hessianfree.sh
@@ -2,7 +2,7 @@
 RUNS=10
 for dataset in $PLANETOID_DATASETS; do
   for model in GCN GAT; do
-    python planetoid.py --model=$model --baseline=hessian --hessianfree_damping=1.0 --dataset=$dataset --runs=$RUNS --file_name=results/$model-$dataset-hessian
-    python planetoid.py --model=$model --baseline=ggn --dataset=$dataset --runs=$RUNS --file_name=results/$model-$dataset-ggn
+    python planetoid.py --model=$model --baseline=Hessian --hessianfree_damping=1.0 --dataset=$dataset --runs=$RUNS --file_name=results/$model-$dataset-hessian
+    python planetoid.py --model=$model --baseline=GGN --dataset=$dataset --runs=$RUNS --file_name=results/$model-$dataset-ggn
   done
 done
