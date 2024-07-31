2ca0f76f888eac380f52db32db2798aafcda42a6
Simon Fetzel <simonf93@univie.ac.at>
2024-07-31 17:35:03 +0000

    added m-fac run script

---
+++ examples/run-m-fac.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# you need to clone https://github.com/sfetzel/m-fac into the folder MFAC first.
+# git clone https://github.com/sfetzel/M-FAC.git MFAC
+
+PLANETOID_DATASETS="Cora CiteSeer PubMed"
+
+RUNS=10
+for dataset in $PLANETOID_DATASETS; do
+  for model in GCN GAT; do
+    python planetoid.py --model=$model --baseline=M-FAC --mfac_damping=0.1 --dataset=$dataset --runs=$RUNS --file_name=results/$model-$dataset-m-fac
+  done
+done
