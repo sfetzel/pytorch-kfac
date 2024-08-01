57f1c35bdd0007e4c7b16fd551bd121dde17c5e3
Simon Fetzel <simonf93@univie.ac.at>
2024-08-01 08:52:44 +0000

    adapted run scripts

--- examples/run-gridsearch.sh
+++ examples/run-gridsearch.sh
@@ -2,11 +2,12 @@
 
 GRIDSEARCH_RUNS=5
 DATASETS="Cora CiteSeer PubMed"
+COV_UPDATE_FREQ=50
 for dataset in $DATASETS; do
-  python gridsearch.py --model=GCN --hidden_channels=64 --dataset=$dataset --runs=$GRIDSEARCH_RUNS
+  python gridsearch.py --cov_update_freqs $COV_UPDATE_FREQ --model=GCN --hidden_channels=64 --dataset=$dataset --runs=$GRIDSEARCH_RUNS
 done
 for dataset in $DATASETS; do
-  python gridsearch.py --model=GAT --hidden_channels=8 --heads=8 --dataset=$dataset --runs=$GRIDSEARCH_RUNS
+  python gridsearch.py --cov_update_freqs $COV_UPDATE_FREQ --model=GAT --hidden_channels=8 --heads=8 --dataset=$dataset --runs=$GRIDSEARCH_RUNS
 done
 
 #python gridsearch.py --epochs=300 --model=GCN --dataset=CiteSeer --runs=$GRIDSEARCH_RUNS --cov_ema_decays 0.0 0.25 --lrs 0.1 0.005 --kfac_dampings 0.1 0.01 0.001
--- examples/run-tudataset.sh
+++ examples/run-tudataset.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
-DAMPING="0.0002 None"
-python comparison.py --kfac_damping $DAMPING --device=cuda:1 --epochs=200 --dataset_name=PROTEINS --model=GIN > comparison-gin-proteins.txt;
-python comparison.py --kfac_damping $DAMPING --device=cuda:1 --epochs=200 --dataset_name=ENZYMES --model=GIN > comparison-gin-enzymes.txt;
+DAMPING="0.1 None"
+DEVICE=cuda:0
+EPOCHS=1000
+PATIENCE=500
 
-python comparison.py --kfac_damping $DAMPING --device=cuda:1 --epochs=200 --dataset_name=MUTAG --model=GIN > comparison-gin-mutag.txt;
-python comparison.py --kfac_damping $DAMPING --device=cuda:1 --epochs=200 --dataset_name=COLLAB --model=GIN > comparison-gin-collab.txt;
-python comparison.py --kfac_damping $DAMPING --device=cuda:1 --epochs=200 --dataset_name=IMDB-BINARY --model=GIN > comparison-gin-imdb-binary.txt;
-python comparison.py --kfac_damping $DAMPING --device=cuda:1 --epochs=200 --dataset_name=IMDB-MULTI --model=GIN > comparison-gin-imdb-multi.txt;
-python comparison.py --kfac_damping $DAMPING --device=cuda:1 --epochs=200 --dataset_name=DD --model=GIN > comparison-gin-dd.txt
+python comparison.py --kfac_damping $DAMPING --device=$DEVICE --epochs=$EPOCHS --patience $PATIENCE --dataset_name=PROTEINS --model=GIN > comparison-gin-proteins.txt;
+python comparison.py --kfac_damping $DAMPING --device=$DEVICE --epochs=$EPOCHS --patience $PATIENCE --dataset_name=ENZYMES --model=GIN > comparison-gin-enzymes.txt;
+
+python comparison.py --kfac_damping $DAMPING --device=$DEVICE --epochs=$EPOCHS --patience $PATIENCE --dataset_name=MUTAG --model=GIN > comparison-gin-mutag.txt;
+python comparison.py --kfac_damping $DAMPING --device=$DEVICE --epochs=$EPOCHS --patience $PATIENCE --dataset_name=COLLAB --model=GIN > comparison-gin-collab.txt;
+python comparison.py --kfac_damping $DAMPING --device=$DEVICE --epochs=$EPOCHS --patience $PATIENCE --dataset_name=IMDB-BINARY --model=GIN > comparison-gin-imdb-binary.txt;
+python comparison.py --kfac_damping $DAMPING --device=$DEVICE --epochs=$EPOCHS --patience $PATIENCE --dataset_name=IMDB-MULTI --model=GIN > comparison-gin-imdb-multi.txt;
+python comparison.py --kfac_damping $DAMPING --device=$DEVICE --epochs=$EPOCHS --patience $PATIENCE --dataset_name=DD --model=GIN > comparison-gin-dd.txt
+python comparison.py --kfac_damping $DAMPING --device=$DEVICE --layer 5  --epochs=$EPOCHS --patience $PATIENCE --dataset_name=REDDIT-BINARY --model=GIN > comparison-gin-reddit-binary-5-layers.txt
 
 
-python comparison.py --kfac_damping $DAMPING --device=cuda:1 --epochs=200 --dataset_name=PROTEINS --model=GAT > comparison-gat-proteins.txt;
-python comparison.py --kfac_damping $DAMPING --device=cuda:1 --epochs=200 --dataset_name=ENZYMES --model=GAT > comparison-gat-enzymes.txt;
-python comparison.py --kfac_damping $DAMPING --device=cuda:1 --epochs=200 --dataset_name=MUTAG --model=GAT > comparison-gat-mutag.txt;
-python comparison.py --kfac_damping $DAMPING --device=cuda:1 --epochs=200 --dataset_name=COLLAB --model=GAT > comparison-gat-collab.txt;
-python comparison.py --kfac_damping $DAMPING --device=cuda:1 --epochs=200 --dataset_name=IMDB-BINARY --model=GAT > comparison-gat-imdb-binary.txt;
-python comparison.py --kfac_damping $DAMPING --device=cuda:1 --epochs=200 --dataset_name=IMDB-MULTI --model=GAT > comparison-gat-imdb-multi.txt;
-python comparison.py --kfac_damping $DAMPING --device=cuda:1 --epochs=200 --dataset_name=DD --model=GAT > comparison-gat-dd.txt
+python comparison.py --kfac_damping $DAMPING --device=$DEVICE --epochs=$EPOCHS --patience $PATIENCE --dataset_name=PROTEINS --model=GAT > comparison-gat-proteins.txt;
+python comparison.py --kfac_damping $DAMPING --device=$DEVICE --epochs=$EPOCHS --patience $PATIENCE --dataset_name=ENZYMES --model=GAT > comparison-gat-enzymes.txt;
+python comparison.py --kfac_damping $DAMPING --device=$DEVICE --epochs=$EPOCHS --patience $PATIENCE --dataset_name=MUTAG --model=GAT > comparison-gat-mutag.txt;
+#python comparison.py --kfac_damping $DAMPING --device=$DEVICE --epochs=$EPOCHS --patience $PATIENCE --dataset_name=COLLAB --model=GAT > comparison-gat-collab.txt;
+#python comparison.py --kfac_damping $DAMPING --device=$DEVICE --epochs=$EPOCHS --patience $PATIENCE --dataset_name=IMDB-BINARY --model=GAT > comparison-gat-imdb-binary.txt;
+#python comparison.py --kfac_damping $DAMPING --device=$DEVICE --epochs=$EPOCHS --patience $PATIENCE --dataset_name=IMDB-MULTI --model=GAT > comparison-gat-imdb-multi.txt;
+python comparison.py --kfac_damping $DAMPING --device=$DEVICE --epochs=$EPOCHS --patience $PATIENCE --dataset_name=DD --model=GAT > comparison-gat-dd.txt
 
-#python comparison.py --kfac_damping $DAMPING --device=cuda:1 --epochs=200 --dataset_name=REDDIT-BINARY --model=GAT > comparison-gat-reddit-binary.txt
-#python comparison.py --kfac_damping $DAMPING --device=cuda:1 --epochs=200 --dataset_name=REDDIT-BINARY --model=GIN > comparison-gin-reddit-binary.txt
+#python comparison.py --kfac_damping $DAMPING --device=$DEVICE --epochs=$EPOCHS --patience $PATIENCE --dataset_name=REDDIT-BINARY --model=GAT > comparison-gat-reddit-binary.txt
 
