271b9d04719e0e469486f742a3ffa468f3c92190
Simon Fetzel <simonf93@univie.ac.at>
2024-08-05 02:10:49 +0000

    added more m-fac experiments

--- examples/run-m-fac.sh
+++ examples/run-m-fac.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 # you need to clone https://github.com/sfetzel/m-fac into the folder MFAC first.
@@ -9,5 +10,6 @@ RUNS=10
 for dataset in $PLANETOID_DATASETS; do
   for model in GCN GAT; do
     python planetoid.py --model=$model --baseline=M-FAC --mfac_damping=0.1 --dataset=$dataset --runs=$RUNS --file_name=results/$model-$dataset-m-fac
+    python planetoid.py --cov_update_freq=50 --model=$model --baseline=M-FAC --mfac_damping=0.1 --dataset=$dataset --runs=$RUNS --file_name=results/$model-$dataset-m-fac-reduced-f
   done
 done
